1223f403dcfcd2cc927c0ae880e320ce09fa15b0
prozz <prozz@gmail.com>
2012-05-28 01:03:34 +0200

    count liberties

--- src/baduk/core.clj
+++ src/baduk/core.clj
@@ -17,3 +17,20 @@
      (= 0 (mod (+ pos 1) size)) true
      (= 0 (mod pos size)) true
      :else false)))
+
+(defn is-corner?
+  [board pos]
+  (let [size (board-size board)]
+    (cond
+     (= pos 0) true
+     (= pos (- size 1)) true
+     (= pos (- (* size size) 1)) true
+     (= pos (- (* size size) size)) true
+     :else false)))
+
+(defn count-liberties
+  [board pos]
+  (cond
+   (is-corner? board pos) 2
+   (is-edge? board pos) 3
+   :else 4))
--- test/baduk/test/core.clj
+++ test/baduk/test/core.clj
@@ -3,16 +3,37 @@
   (:use [clojure.test]))
 
 (deftest board-properties
-  (is (vector? (board 3)))
-  (is (= 9 (count (board 3))))
-  (is (= 3 (board-size (board 3))))
-  (is (= 1 (count (distinct (board 3)))))
-  (is (= \. (first (distinct (board 3))))))
+  (let [board (board 3)]
+  (is (vector? board))
+  (is (= 9 (count board)))
+  (is (= 3 (board-size board)))
+  (is (= 1 (count (distinct board))))
+  (is (= \. (first (distinct board))))))
 
 (deftest edges-checking
   (testing "3x3 board"
-    (are [pos] (true? (is-edge? (board 3) pos)) 0 1 2 3 5 6 7 8)
-    (are [pos] (false? (is-edge? (board 3) pos)) 4))
+    (let [board (board 3)]
+      (are [pos] (true? (is-edge? board pos)) 0 1 2 3 5 6 7 8)
+      (are [pos] (false? (is-edge? board pos)) 4)))
   (testing "4x4 board"
-    (are [pos] (true? (is-edge? (board 4) pos)) 0 1 2 3 4 7 8 11 12 13 14 15)
-    (are [pos] (false? (is-edge? (board 4) pos)) 5 6 9 10)))
+    (let [board (board 4)]
+      (are [pos] (true? (is-edge? board pos)) 0 1 2 3 4 7 8 11 12 13 14 15)
+      (are [pos] (false? (is-edge? board pos)) 5 6 9 10))))
+
+(deftest corner-checking
+  (testing "3x3 board"
+    (let [board (board 3)]
+      (are [pos] (true? (is-corner? board pos)) 0 2 6 8)
+      (are [pos] (false? (is-corner? board pos)) 1 3 4 5 7)))
+  (testing "4x4 board"
+    (let [board (board 4)]
+      (are [pos] (true? (is-corner? board pos)) 0 3 12 15)
+      (are [pos] (false? (is-corner? board pos)) 1 2 4 5 6 7 8 9 10 11 13 14))))
+
+
+(deftest liberties-counting
+  (testing "alone stones on 3x3"
+    (let [board (board 3)]
+      (are [pos] (= 2 (count-liberties board pos)) 0 2 6 8)
+      (are [pos] (= 3 (count-liberties board pos)) 1 3 5 7)
+      (is (= 4 (count-liberties board 4))))))
